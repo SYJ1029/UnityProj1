b912bdf2392ffdf41839ea1ea13cad7d349ca34f
SYJ1029 <abg0986@gmail.com>
2025-05-28 17:52:09 +0900

    기능 분리

--- Assets/scripts/CharacterMove.cs
+++ Assets/scripts/CharacterMove.cs
@@ -7,22 +7,19 @@ public class CharacterMove : MonoBehaviour
     KeyCode prevKey;
 
     public float moveForce;
-    public float mouseSensitivity;
+    public float maxSpeed;
+
     public float jumpForce;
     float jumpSpeed;
-
-    public float maxSpeed;
     private Vector3 accel = Vector3.zero;
 
-    float xRotation = 0f;
-    float yRotation = 0f;
+
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
-        Cursor.lockState = CursorLockMode.Locked;
 
-        jumpSpeed = jumpForce;
+        jumpSpeed = 0f;
     }
 
     void FixedUpdate()
@@ -33,27 +30,8 @@ public class CharacterMove : MonoBehaviour
     void Update()
     {
 
-        float mouseX = Input.GetAxisRaw("Mouse X");
-        float mouseY = Input.GetAxisRaw("Mouse Y");
 
-        // Deadzone 적용
-        if (Mathf.Abs(mouseX) < 0.01f) mouseX = 0f;
-        if (Mathf.Abs(mouseY) < 0.01f) mouseY = 0f;
-
-        // 민감도 적용 + 프레임 보정
-        mouseX *= mouseSensitivity * Time.deltaTime;
-        mouseY *= mouseSensitivity * Time.deltaTime;
-
-        // 누적
-        yRotation += mouseX;
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
-        // 상하 회전: 카메라에만
-        Camera.main.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-
-        // 좌우 회전: 플레이어 바디에만
-        transform.rotation = Quaternion.Euler(0f, yRotation, 0f);
+       
 
 
 
@@ -78,7 +56,7 @@ public class CharacterMove : MonoBehaviour
 
 
        
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && controller.transform.position.y == accel.y)
         {
             jumpSpeed = jumpForce;
         }
@@ -104,7 +82,7 @@ public class CharacterMove : MonoBehaviour
         }
 
 
-            controller.Move(accel * Time.deltaTime);
+        controller.Move(accel * Time.deltaTime);
 
 
         accel = Vector3.zero;
---
+++ Assets/scripts/MouseMove.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class MouseMove : MonoBehaviour
+{
+
+    public float mouseSensitivity;
+
+    float xRotation = 0f;
+    float yRotation = 0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float mouseX = Input.GetAxisRaw("Mouse X");
+        float mouseY = Input.GetAxisRaw("Mouse Y");
+
+        // Deadzone 적용
+        if (Mathf.Abs(mouseX) < 0.01f) mouseX = 0f;
+        if (Mathf.Abs(mouseY) < 0.01f) mouseY = 0f;
+
+        // 민감도 적용 + 프레임 보정
+        mouseX *= mouseSensitivity * Time.deltaTime;
+        mouseY *= mouseSensitivity * Time.deltaTime;
+
+        // 누적
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // 상하 회전: 카메라에만
+        Camera.main.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        // 좌우 회전: 플레이어 바디에만
+        transform.rotation = Quaternion.Euler(0f, yRotation, 0f);
+    }
+}
